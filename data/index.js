c83f22a7a546fc274a5bc6bc5b28ccc6bb976f3d
YORO007xR5R5 <95130086+YORO007xR5R5@users.noreply.github.com>
2026-03-27 11:25:02 +0700

    Add files via upload

--- data/index.js
+++ data/index.js
@@ -3,6 +3,7 @@ import { fruits } from "./fruits.js";
 import { animals } from "./animals.js";
 import { colors } from "./colors.js";
 import { numbers } from "./numbers.js";
+import { hiraganaAIUEO } from "./hiraganaAIUEO.js";
 // import อันอื่นเพิ่มได้
 
 export const categories = {
@@ -10,5 +11,6 @@ export const categories = {
   fruits,
   animals,
   colors,
-  numbers
+  numbers,
+  hiraganaAIUEO,
 };
