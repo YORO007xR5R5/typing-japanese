80cab4a23277082e54812e417cdfde2c4c918bf8
YORO007xR5R5 <95130086+YORO007xR5R5@users.noreply.github.com>
2026-03-27 11:31:37 +0700

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,16 +1,18 @@
+import { hiraganaAIUEO } from "./hiraganaAIUEO.js";
 import { greetings } from "./greetings.js";
 import { fruits } from "./fruits.js";
 import { animals } from "./animals.js";
 import { colors } from "./colors.js";
 import { numbers } from "./numbers.js";
-import { hiraganaAIUEO } from "./hiraganaAIUEO.js";
+
 // import อันอื่นเพิ่มได้
 
 export const categories = {
+   hiraganaAIUEO,
   greetings,
   fruits,
   animals,
   colors,
-  numbers,
-  hiraganaAIUEO
+  numbers
+ 
 };
