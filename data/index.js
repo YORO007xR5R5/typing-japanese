9439acd3dacee64d5679b25717e2ede9a9da64ab
YORO007xR5R5 <95130086+YORO007xR5R5@users.noreply.github.com>
2026-03-28 16:31:20 +0700

    Add files via upload

--- data/index.js
+++ data/index.js
@@ -20,6 +20,24 @@ import { times } from "./times.js";
 import { vegetables } from "./vegetables.js";
 import { weather } from "./weather.js";
 
+import { adjectives } from "./adjectives.js";
+import { adverbs } from "./adverbs.js";
+import { culture } from "./culture.js";
+import { directions } from "./directions.js";
+import { jobs } from "./jobs.js";
+import { nature } from "./nature.js";
+import { objects } from "./objects.js";
+import { office } from "./office.js";
+import { places } from "./places.js";
+import { school } from "./school.js";
+import { seasons } from "./seasons.js";
+import { shopping } from "./shopping.js";
+import { transport } from "./transport.js";
+import { travel } from "./travel.js";
+import { trchnology } from "./trchnology.js";
+import { verbs } from "./verbs.js";
+
+
 export const categories = {
   greetings,
   fruits,
@@ -38,5 +56,23 @@ export const categories = {
   sports,
   times,
   vegetables,
-  weather
+  weather,
+
+  adjectives,
+  adverbs,
+  culture,
+  directions,
+  jobs,
+  nature,
+  objects,
+  office,
+  places,
+  school,
+  seasons,
+  shopping,
+  transport,
+  travel,
+  trchnology,
+  verbs
 };
+
